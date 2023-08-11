b2edc9e793f3418798b53bb92e646ac4aa2dd0f5
Volodymyr <120558476+MrRich0@users.noreply.github.com>
2023-08-11 12:18:13 +0300

    Add files via upload

---
+++ src/main/java/com/example/Module14DevApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module14DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module14DevApplication.class, args);
+	}
+
+}
